d4f3c5948662d2c95010545cd4258aca4cfcf551
kavya410004 <116058046+kavya410004@users.noreply.github.com>
2024-04-30 17:59:24 +0530

    Update controller.js users

--- controller.js
+++ controller.js
@@ -6,7 +6,7 @@ import sharp from "sharp";
 
 export async function createTables(){
   try{
-    await db.query("CREATE TABLE users(name_ VARCHAR(100) NOT NULL, phone_number VARCHAR(10) PRIMARY KEY NOT NULL CHECK (phone_number ~ '^\d{10}$'), user_password TEXT NOT NULL);");
+    //await db.query("CREATE TABLE users(name_ VARCHAR(100) NOT NULL, phone_number VARCHAR(10) PRIMARY KEY NOT NULL CHECK (phone_number ~ '^\d{10}$'), user_password TEXT NOT NULL);");
     await db.query("CREATE TABLE admins(admin_id VARCHAR(5) PRIMARY KEY NOT NULL,	admin_password TEXT NOT NULL);");
     await db.query("CREATE TABLE vehicles(vehicle_id SERIAL PRIMARY KEY NOT NULL, vehicle_model VARCHAR(50) UNIQUE NOT NULL, launched_year INTEGER NOT NULL, seating_capacity INTEGER NOT NULL, milage NUMERIC NOT NULL, price_per_day NUMERIC NOT NULL, transmission_type VARCHAR(50) NOT NULL, vehicle_type VARCHAR(50) NOT NULL, vehicle_image_path TEXT NOT NULL, availability VARCHAR(1) DEFAULT 'T');");
     await db.query("CREATE TABLE drivers(driver_id SERIAL PRIMARY KEY NOT NULL, driver_name VARCHAR(50) UNIQUE NOT NULL, age INTEGER NOT NULL, gender VARCHAR(1) NOT NULL, experience INTEGER NOT NULL, drivable_vehicles VARCHAR(20) NOT NULL, price_per_day NUMERIC NOT NULL, driver_image_path TEXT NOT NULL, availability VARCHAR(1) DEFAULT 'T');");
